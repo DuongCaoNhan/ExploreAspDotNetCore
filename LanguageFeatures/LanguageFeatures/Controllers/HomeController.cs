3cd181c3f96e75f4a198ae4dff146c2107c65db7
NhanDuong <nhan9495@gmail.com>
2020-12-30 01:02:02 +0700

    Using the Null Conditional Operator

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features" });
+            //The null conditional operator allows for null values to be detected more elegantly(thanh lich) :).
+            List<string> results = new List<string>(); foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+            }
+            return View(results);
+            // return View(new string[] { "C#", "Language", "Features" });
         }
     }
 }
