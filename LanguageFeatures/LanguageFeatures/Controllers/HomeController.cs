4ebe461e792f6bbd8ee8f6bed118b31fa4ff9300
NhanDuong <nhan9495@gmail.com>
2020-12-30 01:10:23 +0700

    Chaining the Null Conditional Operator

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -8,11 +8,14 @@ namespace LanguageFeatures.Controllers
         public ViewResult Index()
         {
             //The null conditional operator allows for null values to be detected more elegantly(thanh lich) :).
-            List<string> results = new List<string>(); foreach (Product p in Product.GetProducts())
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
             {
                 string name = p?.Name;
                 decimal? price = p?.Price;
-                results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+                string relatedName = p?.Related?.Name; 
+                // results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
             }
             return View(results);
             // return View(new string[] { "C#", "Language", "Features" });
