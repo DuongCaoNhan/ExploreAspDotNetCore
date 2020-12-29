a41ef067cbf2262e5cecc6636b18ddaa0ff6127d
NhanDuong <nhan9495@gmail.com>
2020-12-30 01:14:01 +0700

    Combining the Conditional and Coalescing Operators

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -11,9 +11,15 @@ namespace LanguageFeatures.Controllers
             List<string> results = new List<string>();
             foreach (Product p in Product.GetProducts())
             {
-                string name = p?.Name;
-                decimal? price = p?.Price;
-                string relatedName = p?.Related?.Name; 
+                // string name = p?.Name;
+                // decimal? price = p?.Price;
+                // string relatedName = p?.Related?.Name; 
+
+                //Combining the Conditional and Coalescing Operators
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+
                 // results.Add(string.Format("Name: {0}, Price: {1}", name, price));
                 results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
             }
