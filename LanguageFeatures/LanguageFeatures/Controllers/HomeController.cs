8e04721cb308d1133817e7ed7c807657277488d5
NhanDuong <nhan9495@gmail.com>
2021-03-02 07:43:24 +0700

    Applying Extension Methods to an Interface

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -50,8 +50,8 @@ namespace LanguageFeatures.Controllers
             // return View("Index", products.Keys);
 
             //Pattern matching
-            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
-            decimal total = 0;
+            // object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
+            // decimal total = 0;
             // for (int i = 0; i < data.Length; i++)
             // {
             //     if (data[i] is decimal d)
@@ -63,17 +63,33 @@ namespace LanguageFeatures.Controllers
             // return View("Index", new string[] { $"Total: {total:C2}" });
 
             //Pattern Matching in switch Statements
-            for (int i = 0; i < data.Length; i++) { 
-                switch (data[i]) { 
-                    case decimal decimalValue: 
-                        total += decimalValue; 
-                        break; 
-                    case int intValue when intValue > 50:
-                        total += intValue; 
-                        break;
-                }
-            }
-            return View("Index", new string[] { $"Total: {total:C2}" });
+            // for (int i = 0; i < data.Length; i++) { 
+            //     switch (data[i]) { 
+            //         case decimal decimalValue: 
+            //             total += decimalValue; 
+            //             break; 
+            //         case int intValue when intValue > 50:
+            //             total += intValue; 
+            //             break;
+            //     }
+            // }
+            // return View("Index", new string[] { $"Total: {total:C2}" });
+            // ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+
+            // Product[] productArray = { new Product {Name = "Kayak", Price = 275M},
+            //                            new Product {Name = "Lifejacket", Price = 48.95M} };
+            // decimal cartTotal = cart.TotalPrices();
+            // decimal arrayTotal = productArray.TotalPrices();
+            // return View("Index", new string[] { 
+            //     $"Cart Total: {cartTotal:C2}", 
+            //     $"Array Total: {arrayTotal:C2}" });
+                Product[] productArray = { 
+                    new Product {Name = "Kayak", Price = 275M}, 
+                    new Product {Name = "Lifejacket", Price = 48.95M}, 
+                    new Product {Name = "Soccer ball", Price = 19.50M}, 
+                    new Product {Name = "Corner flag", Price = 34.95M} }; 
+                    decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices(); 
+                    return View("Index", new string[] { $"Array Total: {arrayTotal:C2}" });
         }
     }
 }
