8f71039713326320f1c0a0cf26d73a16403bad87
DuongCaoNhan <nhan9495@gmail.com>
2021-06-04 17:32:34 +0700

    Adding entity
    Adding entity,

---
+++ EFCore5WebApp/EFCore5WebApp.Core/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCore5WebApp.Core
+{
+    class Person
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
